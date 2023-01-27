1eee0a23a9530a738994690111b864579e706440
Gellert19 <goldendemonfoxgg@outlook.com>
2023-01-27 12:20:54 +0000

    First

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+const string FILE = @"C:\Users\golde\Downloads\Plain.txt";
+
+sr = new streamreader(FILE);
+
+var data = list<string>();
+
+int[] tally = new int[26];
+
+
+
+while (!sr.endofstream)
+{
+    data.Add(sr.ReadLine());
+
+    foreach(var line in data)
+    {
+        foreach (var ch in line)
+        {
+            tally[(int)ch  - 65]++;
+        }
+    }
+}
