12352b94bf57066f215e8d3885c375e86e04a8ba
Gellert19 <goldendemonfoxgg@outlook.com>
2023-02-02 09:56:13 +0000

    removed the csv

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 class Cipher
 {
-    static void Main()
+    static void Ciph()
     {
         const string FILE = @"C:\Users\golde\Downloads\Plain.txt";
         const string SCHL = @"C:\Users\OliverDixon\Downloads\Plain.txt";
@@ -36,40 +36,6 @@ class Cipher
         {
             Console.Write(tally[i] + ",");
         }
-
-        Console.ReadLine();
-    }
-}
-
-
-class CSVThings
-{
-    class Book
-    {
-        public string title;
-        public int year;
-        public double price;
-    }
-
-    static void Bookie()
-    {
-        Book book;
-        List<Book> books = new List<Book>();
-        StreamReader sr = new StreamReader(@"C:\Users\OliverDixon\Downloads\somedata.csv");
-        
-        while(!sr.EndOfStream)
-        {
-            string line = sr.ReadLine()!;
-            string[] data = line.Split(',');
-
-            book = new Book();
-            book.title = data[0];
-            book.year = int.Parse(data[1]);
-            book.price = double.Parse(data[2]);
-            books.Add(book);
-        }
-
-        Console.WriteLine(books);
 
         Console.ReadLine();
     }
